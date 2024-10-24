d02ee23b7fb9d4e495cfee6886f9a9fb1d71da12
Henrique <henriqueferreira0320@gmail.com>
2024-10-24 13:13:55 -0300

    Módulo 5 - Aula 04

---
+++ modulo_5_php_basico/aula_04/index.php
@@ -0,0 +1,21 @@
+<?php 
+    /* String */
+    $nome = 'Henrique';
+    
+    /* Number */
+    $idade = 20;
+    
+    /* Boolean */
+    $casado = true;
+
+    /* Doubles */
+    $altura = 1.75;
+
+
+    /* Agora vou printar na tela minhas 2 variaveis */
+    echo 'Meu nome é '.$nome.' e minha idade é '.$idade.' anos. </br>';
+
+    echo $casado;
+    echo '</br>';
+    echo $altura;
+?>
