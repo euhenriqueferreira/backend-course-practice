7b48c200f35a74e331fdfdc3fa87288dfdcde9fc
Henrique <henriqueferreira0320@gmail.com>
2024-10-24 11:55:23 -0300

    Início módulo PHP Básico

---
+++ modulo_5_php_basico/aula_01/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Minha página HTML</title>
+    </head>
+
+    <body>
+            <p>
+                <?php
+                    echo 'Olá mundo'
+                ?>
+            </p>
+    </body>
+</html>
