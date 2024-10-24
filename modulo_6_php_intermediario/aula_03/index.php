189f6cde13dc00fa3b8f09c85701b0648b4cf212
Henrique <henriqueferreira0320@gmail.com>
2024-10-24 14:58:16 -0300

    Módulo 6 - Aula 03

---
+++ modulo_6_php_intermediario/aula_03/index.php
@@ -0,0 +1,21 @@
+<?php 
+    // sleep() -> Dormir o código por X segundos
+    // sleep(3);
+
+    // echo 'Hehe';
+    
+    // sleep(3);
+
+    // echo 'hehehe';
+
+    $nome = 'Clinton';
+
+    if($nome == 'Henrique'){
+        echo 'Tudo certo';
+    } else{
+        die('O script parou de ser executado');
+    }
+
+
+    echo 'TESTEEEEEEEE';
+?>
