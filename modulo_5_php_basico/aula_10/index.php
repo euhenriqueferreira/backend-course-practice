41754c7281f5ff4822d1ccee1d099eb6db0158f1
Henrique <henriqueferreira0320@gmail.com>
2024-10-24 14:08:31 -0300

    Módulo 5 - Aula 10

---
+++ modulo_5_php_basico/aula_10/index.php
@@ -0,0 +1,24 @@
+<?php 
+    $nome = 'Henrique';
+    $nome2 = 'Lucas';
+
+    $valor = 5;
+    $valor2 = '5';
+
+    // = -> atribuição
+    // == -> comparação de valor
+
+    // if($nome == $nome2){
+    //     echo 'verdade';
+    // }
+
+    // if($nome != $nome2){
+    //     echo 'diferente';
+    // }
+
+    if($valor === $valor2){
+        echo 'verdade';
+    } else{
+        echo 'falso';
+    }
+?>
