50e259cb7572fe862b90c6664a2239b061c3aa7f
Henrique <henriqueferreira0320@gmail.com>
2024-10-24 14:49:31 -0300

    Módulo 6 - Aula 02

---
+++ modulo_6_php_intermediario/aula_02/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+    $arr = ['Henrique', 'João'];
+
+    $arr2 = array(array('Henrique', 'Eduardo'), array(20, 21));
+
+    $arr3 = [['Livros', 'Jogos', 'Musicas'], ['Harry Potter', 'GTA V', 'Matue']];
+
+    echo $arr2[1][1];
+
+    echo $arr3[1][2]
+?>
