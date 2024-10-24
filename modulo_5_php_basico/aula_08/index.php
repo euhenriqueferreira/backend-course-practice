7725c56193667030c3e5c08639342ca9ec19bbc7
Henrique <henriqueferreira0320@gmail.com>
2024-10-24 13:50:27 -0300

    Módulo 5 - Aula 08

---
+++ modulo_5_php_basico/aula_08/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+$number = 5 + 5;
+$number2 = 10 / 2;
+$resultado = $number * $number2;
+
+echo $number;
+echo '</br>';
+echo $number2;
+echo '</br>';
+echo 'O resultado é '.$resultado;
+
+$expressao = 10 + 20 - 42 + 67;
+
+echo '</br>';
+echo 'O resultado da expressão é '.$expressao;
+
+$expressao2 = 10 + 20 * 2;
+echo '</br>';
+echo 'O resultado da expressão 2 é '.$expressao2;
+?>
